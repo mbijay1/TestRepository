dc99f42b2c48c60b466905aff25a258387a7b974
mbijay <bj.manandhar@gmail.com>
2017-09-25 00:03:20 -0500

    Initial Checkin for Test project

---
+++ Test/src/Test.java
@@ -0,0 +1,30 @@
+
+public class Test {
+	int a =10;
+	int b=20;
+	static int c =30;
+	static int d=40;
+	void m1(){
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(Test.c);
+		System.out.println(Test.d);
+		
+	}
+	static void m2(){
+		Test t = new Test();
+		System.out.println(t.a);
+		System.out.println(t.b);
+		System.out.println(Test.c);
+		System.out.println(Test.d);
+	}
+
+	public static void main(String[] args) {
+		Test t1= new Test();
+		t1.m1();
+		Test.m2();
+		
+
+	}
+
+}
